cfffb136665f03ff04696234d68eb87817f2dd65
Schertzpants <112591868+Schertzpants@users.noreply.github.com>
2024-05-03 13:36:10 -0400

    Update dmem.sv

--- catalog/dmem/dmem.sv
+++ catalog/dmem/dmem.sv
@@ -1,40 +1,42 @@
-//////////////////////////////////////////////////////////////////////////////////
-// The Cooper Union
-// ECE 251 Spring 2024
-// Engineer: Prof Rob Marano
-// 
-//     Create Date: 2023-02-07
-//     Module Name: dmem
-//     Description: 32-bit RISC memory ("data" segment)
-//
-// Revision: 1.0
-//
-//////////////////////////////////////////////////////////////////////////////////
-`ifndef DMEM
-`define DMEM
+`ifndef DATA_MEMORY
+`define DATA_MEMORY
 
-`timescale 1ns/100ps
-
-module dmem
-// n=bit length of register; r=bit length of addr to limit memory and not crash your verilog emulator
-    #(parameter n = 32, parameter r = 6)(
-    //
-    // ---------------- PORT DEFINITIONS ----------------
-    //
-    input  logic           clk, write_enable,
-    input  logic [(n-1):0] addr, writedata,
-    output logic [(n-1):0] readdata
+module data_memory (
+    input clk,
+    input [15:0] mem_access_addr,
+    input [15:0] mem_write_data,
+    input mem_write_en,
+    input mem_read,
+    output reg [15:0] mem_read_data
 );
-    //
-    // ---------------- MODULE DESIGN IMPLEMENTATION ----------------
-    //
-    logic [(n-1):0] RAM[0:(2**r-1)];
 
-    assign readdata = RAM[addr[(n-1):2]]; // word aligned (ignores lower 2 bits of address)
+    // Declare memory array
+    reg [15:0] ram [255:0];
 
-    always @(posedge clk) // write on posedge
-        if (write_enable) RAM[addr[(n-1):2]] <= writedata;
+    // Extract lower 8 bits of the memory address for indexing
+    wire [7:0] ram_addr = mem_access_addr[7:0];
+
+    // Initialize memory to zeros
+    integer i;
+    initial begin
+        for (i = 0; i < 256; i = i + 1)
+            ram[i] = 16'd0;
+    end
+
+    // Write to memory on positive edge of clock if mem_write_en is asserted
+    always @(posedge clk) begin
+        if (mem_write_en)
+            ram[ram_addr] <= mem_write_data;
+    end
+
+    // Read from memory if mem_read is asserted
+    always @* begin
+        if (mem_read)
+            mem_read_data = ram[ram_addr];
+        else
+            mem_read_data = 16'd0;
+    end
 
 endmodule
 
-`endif // DMEM
+`endif // DATA_MEMORY
