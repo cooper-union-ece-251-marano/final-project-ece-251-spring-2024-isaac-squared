778678f281cef09c902f16bd6fb8d99d822e9315
Schertzpants <112591868+Schertzpants@users.noreply.github.com>
2024-05-05 16:46:09 -0400

    Update imem.sv

--- catalog/imem/imem.sv
+++ catalog/imem/imem.sv
@@ -1,39 +1,43 @@
-`ifndef instr_mem
-`define instr_mem
+//////////////////////////////////////////////////////////////////////////////////
+// The Cooper Union
+// ECE 251 Spring 2024
+// Engineer: Prof Rob Marano
+// 
+//     Create Date: 2023-02-07
+//     Module Name: imem
+//     Description: 32-bit RISC memory (instruction "text" segment)
+//
+// Revision: 1.0
+//
+//////////////////////////////////////////////////////////////////////////////////
+`ifndef IMEM
+`define IMEM
 
-module instr_mem (
-    input [15:0] pc,
-    output reg [15:0] instruction
+`timescale 1ns/100ps
+
+module imem
+// n=bit length of register; r=bit length of addr to limit memory and not crash your verilog emulator
+    #(parameter n = 16, parameter r = 6)(
+    //
+    // ---------------- PORT DEFINITIONS ----------------
+    //
+    input  logic [(r-1):0] addr,
+    output logic [(n-1):0] readdata
 );
+    //
+    // ---------------- MODULE DESIGN IMPLEMENTATION ----------------
+    //
+    logic [(n-1):0] RAM[0:(2**r-1)];
 
-// Enhanced instruction memory code
-reg [15:0] rom[15:0];
-
-// Initialize instruction memory with program code
-initial begin
-    rom[0] = 16'b1000000010000000;  // lw $1,0
-    rom[1] = 16'b1110000100000001;  // addi $2, $0, 1
-    rom[2] = 16'b0010101000100010;  // loop: slti $4,$1,21
-    rom[3] = 16'b1101000001100111;  // beq $4,$0,break
-    rom[4] = 16'b0000010100110000;  // add $3, $1, $2
-    rom[5] = 16'b0000010000100000;  // add $2, $1, $0
-    rom[6] = 16'b0000110000010000;  // add $1, $3, $0
-    rom[7] = 16'b1010101010000000;  // sw $2
-    rom[8] = 16'b1100000001111010;  // beq $0,$0,loop
-    // Fill remaining addresses with no operation
-    for (int i = 9; i < 16; i = i + 1) begin
-        rom[i] = 16'b0000000000000000;
+  initial
+    begin
+      // read memory in hex format from file 
+      // $readmemh("program_exe",RAM);
+      $readmemh("mult-prog_exe",RAM);
     end
-end
 
-// Output the instruction based on PC
-always @(*) begin
-    if (pc[15:0] < 16) begin
-        instruction = rom[pc[4:1]];
-    end else begin
-        instruction = 16'd0;
-    end
-end
+  assign readdata = RAM[addr]; // word aligned
 
 endmodule
-`endif
+
+`endif // IMEM
