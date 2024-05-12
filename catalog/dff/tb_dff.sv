51db05863926d399b8ed5788dafa64ae55bf8700
Schertzpants <112591868+Schertzpants@users.noreply.github.com>
2024-05-11 23:02:48 -0400

    Update tb_dff.sv

--- catalog/dff/tb_dff.sv
+++ catalog/dff/tb_dff.sv
@@ -1,52 +1,43 @@
-`timescale 1ns/1ps
+//////////////////////////////////////////////////////////////////////////////////
+// The Cooper Union
+// ECE 251 Spring 2024
+// Engineer: Prof Rob Marano
+// 
+//     Create Date: 2023-02-07
+//     Module Name: dff
+//     Description: 32 bit D flip flop
+//
+// Revision: 1.0
+//
+//////////////////////////////////////////////////////////////////////////////////
+`ifndef DFF
+`define DFF
 
-module tb_d_ff;
+`timescale 1ns/100ps
 
-// Parameters
-parameter PERIOD = 10; // Clock period
-parameter HALF_PERIOD = PERIOD / 2;
-
-// Signals
-reg d, clk, rst_n;
-wire q;
-
-// Instantiate the D_FF module
-D_FF uut (
-    .q(q),
-    .d(d),
-    .rst_n(rst_n),
-    .clk(clk)
+module dff
+    #(parameter n = 32)(
+    //
+    // ---------------- PORT DEFINITIONS ----------------
+    //
+    input  logic CLOCK, RESET,
+    input  logic [(n-1):0] D,
+    output logic [(n-1):0] Q
 );
-
-// Clock generation
-initial begin
-    clk = 0;
-    forever #HALF_PERIOD clk = ~clk;
-end
-
-// Reset generation
-initial begin
-    rst_n = 0;
-    #100;
-    rst_n = 1;
-end
-
-// Stimulus
-initial begin
-    // Test case 1: d = 0
-    d = 1'b0;
-    #PERIOD;
-    // Test case 2: d = 1
-    d = 1'b1;
-    #PERIOD;
-    // Test case 3: d = 0
-    d = 1'b0;
-    #PERIOD;
-    // Test case 4: d = 1
-    d = 1'b1;
-    #PERIOD;
-    // End simulation
-    $finish;
-end
-
+    //
+    // ---------------- MODULE DESIGN IMPLEMENTATION ----------------
+    //
+    always @(posedge CLOCK, posedge RESET)
+    begin
+        if (RESET)
+        begin
+            Q <= 0;
+        end
+        else
+        begin
+            Q <= D;
+        end
+    end
 endmodule
+
+`endif // DFF
