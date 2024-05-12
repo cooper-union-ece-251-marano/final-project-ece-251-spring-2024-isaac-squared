46316d27272b2794ba9821dbd8fc93c14d53927b
Schertzpants <112591868+Schertzpants@users.noreply.github.com>
2024-05-11 22:56:59 -0400

    Update adder.sv

--- catalog/adder/adder.sv
+++ catalog/adder/adder.sv
@@ -1,11 +1,11 @@
 //////////////////////////////////////////////////////////////////////////////////
 // The Cooper Union
 // ECE 251 Spring 2024
-// Engineer: YOUR NAMES
+// Engineer: Isaac Schertz, Isaac Amar
 // 
 //     Create Date: 2023-02-07
 //     Module Name: adder
-//     Description: simple behavorial adder
+//     Description: simple behavioral adder
 //
 // Revision: 1.0
 //
@@ -13,21 +13,22 @@
 `ifndef ADDER
 `define ADDER
 
-`timescale 1ns/100ps
+`timescale 1ns / 100ps
 
 module adder
-    #(parameter n = 16)  
-    (
-        input [n-1:0] a,  // First operand
-        input [n-1:0] b,  // Second operand
-        output [n-1:0] sum  // Sum of a and b
+    #(parameter n = 32)(
+        //
+        // ---------------- PORT DEFINITIONS ----------------
+        //
+        input [(n-1):0] a, b,
+        output reg [(n-1):0] c
     );
-
     //
     // ---------------- MODULE DESIGN IMPLEMENTATION ----------------
     //
-
-    assign sum = a + b;  // Performs bit-wise addition of a and b
+    always @* begin
+        c = a + b; // Using procedural assignment inside an always block
+    end
 
 endmodule
 
